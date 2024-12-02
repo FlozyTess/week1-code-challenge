05d209d704d6d8e0a60ee1712d96965e8ba92ec2
FlozyTess <cecilrency23@gmail.com>
2024-12-02 08:44:08 +0300

    updated nhif

--- challenge3.js
+++ challenge3.js
@@ -14,3 +14,40 @@ let paye = 0;
     } else {
         paye = (24000 * 0.1) + ((32333 - 24000) * 0.25) + ((500000 - 32333) * 0.3) + ((800000 - 500000) * 0.325) + ((grossSalary - 800000) * 0.35); // 35% for salaries above 800,000
     }
+ // NHIF Deductions
+ let nhif;
+    if (grossSalary <= 5999) {
+        nhif = 150;
+    } else if (grossSalary <= 7999) {
+        nhif = 300;
+    } else if (grossSalary <= 11999) {
+        nhif = 400;
+    } else if (grossSalary <= 14999) {
+        nhif = 500;
+    } else if (grossSalary <= 19999) {
+        nhif = 600;
+    } else if (grossSalary <= 24999) {
+        nhif = 750;
+    } else if (grossSalary <= 29999) {
+        nhif = 850;
+    } else if (grossSalary <= 34999) {
+        nhif = 900;
+    } else if (grossSalary <= 39999) {
+        nhif = 950;
+    } else if (grossSalary <= 44999) {
+        nhif = 1000;
+    } else if (grossSalary <= 49999) {
+        nhif = 1100;
+    } else if (grossSalary <= 59999) {
+        nhif = 1200;
+    } else if (grossSalary <= 69999) {
+        nhif = 1300;
+    } else if (grossSalary <= 79999) {
+        nhif = 1400;
+    } else if (grossSalary <= 89999) {
+        nhif = 1500;
+    } else if (grossSalary <= 99999) {
+        nhif = 1600;
+    } else {
+        nhif = 1700; // Maximum NHIF deduction
+    }
