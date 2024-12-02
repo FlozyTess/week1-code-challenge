2956d83006427f2dc205852b4011953d996e6d43
FlozyTess <cecilrency23@gmail.com>
2024-12-02 04:23:13 +0300

    completed challenge1

--- challenge1.js
+++ challenge1.js
@@ -7,16 +7,15 @@
     else return "E"; // grading system based on marks
  }
  for(let n = 0;n < 3; n++){
-    let marks = prompt("input marks between(0 and 100):");
-    //prompt to input marks
+    let marks = prompt("input marks between(0 and 100):");//prompt to input marks
  
      marks=Number(marks);//converts input from string to number
 
      
      if(isNaN(marks) || marks < 0 || marks > 100){
-        console.log ("Invalid input.Please enter a number between 0 and 100.");//validate the input
-        n--;//decrement the counter to allow the user to input marks again
-        continue;//skip the rest of the loop and prompt again 
+        console.log ("Invalid input. Please enter a number between 0 and 100.");//validate the input
+        n--; //decrements the counter to allow the user to input marks again
+        continue; //skip the rest of the loop and prompt again 
      } 
-       console.log(`Grade: ${calculateGrade(marks)}`);
-    }
+       console.log(`Grade: ${calculateGrade(marks)}`); // if valid,calculate and log the grade
+ }
