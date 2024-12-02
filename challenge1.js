2f468c798611ae1d5e2d2c246e76dca1c3e83700
FlozyTess <cecilrency23@gmail.com>
2024-12-02 03:39:15 +0300

    created a prompt

--- challenge1.js
+++ challenge1.js
@@ -4,5 +4,10 @@
     if (marks >=60) return "B";
     if (marks >=50) return "C";
     if (marks >=40) return "D";
-    else return "E"; 
+    else return "E"; // grading system based on marks
  }
+ for(let n = 0;n < 3; n++){
+    let marks = prompt("input marks between(0 and 100):");
+    //prompt to input marks
+ }
+ 
