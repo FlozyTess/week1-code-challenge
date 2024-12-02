81e17a827d7962fff417504f8828f542cf988e78
FlozyTess <cecilrency23@gmail.com>
2024-12-02 05:09:10 +0300

    updated the files

--- challenge2.js
+++ challenge2.js
@@ -1,6 +1,6 @@
 //Speed detector
 function detectSpeed(speed){
     const speedLimit = 70;
-    const demeritPoint = 12;
-    const 
+    const demeritPointCutoff = 12;
+    const pointPerExcessSpeed = 5;
 }
