0ee7a84e475f22c6650b384a1f35472d6a446d18
FlozyTess <cecilrency23@gmail.com>
2024-12-02 03:23:30 +0300

    created a grade mapping system

--- challenge1.js
+++ challenge1.js
@@ -1,4 +1,8 @@
 // Student Grade Generator
  function calculateGrade(marks) {
-    
+    if (marks > 79) return "A";
+    if (marks >=60) return "B";
+    if (marks >=50) return "C";
+    if (marks >=40) return "D";
+    else return "E"; 
  }
