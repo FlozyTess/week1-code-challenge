71fa392e0cf44a8b080af944bc2bdf54ac486233
FlozyTess <cecilrency23@gmail.com>
2024-12-02 05:21:45 +0300

    check speed limit function

--- challenge2.js
+++ challenge2.js
@@ -3,4 +3,6 @@ function detectSpeed(speed){
     const speedLimit = 70;
     const demeritPointCutoff = 12;
     const pointPerExcessSpeed = 5;
+    //check if speed is below or equal to the speed limit
+    if (speed <= speedLimit)
 }
